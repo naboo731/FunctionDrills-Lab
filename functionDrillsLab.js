4f1c4c9c78b1de56485b42e3828a90f92516ec95
Nichole Boothman <nbooth731@gmail.com>
2021-11-15 18:20:28 -0500

    Completed

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -267,14 +267,13 @@ let sampleArray2 = [0,1,2,3,4,5,6,7,8,9]
 */
 
 //CODE HERE
-
+let value = "True"
 
 function ascendingChecker(array){
  for (i = 0; i < array.length; i++)
   if(array[i] >= array[i + 1]){
-    let value = "False"
-    console.log (array[i], array[i + 1])
-  } else value = "True"
+    value = "False"
+  } 
 return value
 } 
 
